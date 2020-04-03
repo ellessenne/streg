7521f50a74f69e90ee8ddb223ba945e21cd341e3
Alessandro Gasparini <ellessenne@gmail.com>
2020-04-03 16:42:21 +0200

    Started working on predictions

--- testing.R
+++ testing.R
@@ -1,40 +1,15 @@
 ### Testing
 devtools::load_all()
-
-fit <- streg(Surv(failtime, event) ~ 1, data = kva, distribution = "exp")
-logLik(fit)
-AIC(fit)
-AIC(summary(fit))
-BIC(fit)
-BIC(summary(fit))
-
-summary(streg(Surv(studytime, died) ~ 1, data = cancer, dist = "logn"))
-
-
-
-
-
-
-
-
-
 library(simsurv)
 set.seed(9911)
 
-# Weibull model
-covs <- data.frame(id = seq(100000), trt = stats::rbinom(100000, 1L, 0.5))
-s1 <- simsurv(lambdas = 0.1, gammas = 1.5, betas = c(trt = -0.5), x = covs, maxt = 5, dist = "weibull")
-df <- merge(covs, s1)
-summary(streg(Surv(eventtime, status) ~ trt, data = df, dist = "wei"))
-
-# Exponential model
-covs <- data.frame(id = seq(100000), trt = stats::rbinom(100000, 1L, 0.5))
-s1 <- simsurv(lambdas = 0.1, betas = c(trt = -0.5), x = covs, maxt = 5, dist = "exponential")
-df <- merge(covs, s1)
-summary(streg(Surv(eventtime, status) ~ trt, data = df, dist = "wei"))
-
-# Gompertz model
-covs <- data.frame(id = seq(100000), trt = stats::rbinom(100000, 1L, 0.5))
+covs <- data.frame(id = seq(1000), trt = stats::rbinom(1000, 1L, 0.5))
 s1 <- simsurv(lambdas = 0.1, gammas = 1.5, betas = c(trt = -0.5), x = covs, maxt = 5, dist = "gompertz")
 df <- merge(covs, s1)
-summary(streg(Surv(eventtime, status) ~ trt, data = df, dist = "gom"))
+
+fit <- streg(Surv(eventtime, status) ~ trt, data = df, distribution = "gom", x = TRUE)
+
+
+
+nd <- data.frame(trt = 1)
+predict(fit, type = "surv", newdata = nd)
