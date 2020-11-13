72daba2f3eb071f90c0a972a76e6ab37d0467b9b
Alessandro Gasparini <ellessenne@gmail.com>
2020-11-13 08:56:37 +0100

    Improve starting values by fitting the empty model first

--- R/streg.R
+++ R/streg.R
@@ -12,8 +12,20 @@ streg <- function(formula, data, distribution = "exponential", x = FALSE, y = FA
   X <- stats::model.matrix(formula[-2], data = data)
   # Process starting values
   if (is.null(init)) {
-    init <- rep(1, ncol(X) + as.numeric(distribution != "exponential"))
-    init[seq(ncol(X))] <- 0
+    # Fit the empty model here to improve starting values, if not provided with starting values
+    empty_formula <- stats::update(formula, ~ -.)
+    empty_X <- stats::model.matrix(empty_formula[-2], data = data)
+    init <- rep(1, ncol(empty_X) + as.numeric(distribution != "exponential"))
+    f <- switch(distribution,
+      "exponential" = TMB::MakeADFun(data = list(model = "ll_exp", data = empty_X, time = start, status = status), parameters = list(beta = init), silent = TRUE, DLL = "streg_TMBExports"),
+      "weibull" = TMB::MakeADFun(data = list(model = "ll_wei", data = empty_X, time = start, status = status), parameters = list(beta = init[-length(init)], logp = init[length(init)]), silent = TRUE, DLL = "streg_TMBExports"),
+      "gompertz" = TMB::MakeADFun(data = list(model = "ll_gom", data = empty_X, time = start, status = status), parameters = list(beta = init[-length(init)], gamma = init[length(init)]), silent = TRUE, DLL = "streg_TMBExports")
+    )
+    # Fit
+    empty_fit <- stats::nlminb(start = f$par, objective = f$fn, gradient = f$gr, hessian = f$he)
+    init <- rep(0, ncol(X) + as.numeric(distribution != "exponential"))
+    init[1] <- empty_fit$par[1]
+    if (length(empty_fit$par) > 1) init[length(init)] <- empty_fit$par[2]
   }
   names(init) <- switch(distribution,
     "exponential" = colnames(X),
@@ -42,7 +54,7 @@ streg <- function(formula, data, distribution = "exponential", x = FALSE, y = FA
   # Add sum(log(t)) of uncensored observations to log-likelihood
   out$loglik <- -model.fit$objective
   out$n <- nrow(X)
-  out$nevent <- sum(status)
+  out$n.events <- sum(status)
   out$time.at.risk <- sum(start)
   out$distribution <- distribution
   out$convergence <- model.fit$convergence
