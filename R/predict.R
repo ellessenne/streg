74baf6f8affb43d6564e511a17e7e5a42c0df9c4
Alessandro Gasparini <ellessenne@gmail.com>
2020-11-16 17:54:12 +0100

    se.fit of predictions looks good!

--- R/predict.R
+++ R/predict.R
@@ -1,13 +1,14 @@
 #' @importFrom stats predict
 #' @export
 predict.streg <- function(object, newdata = NULL, type = "xb", se.fit = FALSE, ...) {
-  # object <- fit2
+  # object <- fit3
   # newdata <- NULL
   # type <- "xb"
   # se.fit <- FALSE
 
   # Process type of prediction to obtain
   type <- match.arg(arg = type, choices = c("xb", "hazard", "surv"))
+
   # Process newdata
   if (is.null(newdata)) {
     if (is.null(object$x)) {
@@ -16,40 +17,20 @@ predict.streg <- function(object, newdata = NULL, type = "xb", se.fit = FALSE, .
       newdata <- object$x
     }
   }
+
   # Then, create matrix data as it is used by streg for estimation
   S <- .process_streg_formula(formula = object$formula, data = newdata, which = "y")
   start <- S[, which(grepl("^time|^start", colnames(S))), drop = FALSE]
   status <- S[, which(grepl("^status", colnames(S))), drop = FALSE]
   X <- .process_streg_formula(formula = object$formula, data = newdata, which = "x")
+
   # Now, it all depends on whether we want std.errors or not
   if (se.fit) {
-    ### This relies on predictnl
-    ### Needs to be implemented here
+    preds <- rstpm2::predictnl(object, .predictnl_internal, newdata = X, .t = start, .type = type)
   } else {
-    if (type == "xb") {
-      out <- .xb(X = X, par = coef(object))
-    } else {
-      if (object$distribution == "exponential") {
-        if (type == "hazard") {
-          out <- .exp_h(X = X, par = coef(object))
-        } else if (type == "surv") {
-          out <- .exp_surv(X = X, t = start, par = coef(object))
-        }
-      } else if (object$distribution == "weibull") {
-        if (type == "hazard") {
-          out <- .wei_h(X = X, t = start, par = coef(object))
-        } else if (type == "surv") {
-          out <- .wei_surv(X = X, t = start, par = coef(object))
-        }
-      } else if (object$distribution == "gompertz") {
-        if (type == "hazard") {
-          out <- .gom_h(X = X, t = start, par = coef(object))
-        } else if (type == "surv") {
-          out <- .gom_surv(X = X, t = start, par = coef(object))
-        }
-      }
-    }
+    preds <- .predictnl_internal(object = object, newdata = X, .t = start, .type = type)
   }
-  dim(out) <- NULL
-  return(list(X, S, out))
+
+  # Finally, return predictions
+  return(preds)
 }
