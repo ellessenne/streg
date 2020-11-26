27090ee57002e5eac42ac056e128eccc40dd4b6c
Alessandro Gasparini <ellessenne@gmail.com>
2020-11-26 16:06:47 +0100

    Added Stata predictions for comparison before merging branch

---
+++ data-raw/predict-stata.R
@@ -0,0 +1,18 @@
+### Code to prepare internal df for testing: 'predict-stata'
+### Dataset was created using 'predict-stata.do' in the 'data-raw' folder
+library(haven)
+library(usethis)
+
+## Read data
+predict_stata <- haven::read_dta(file = "data-raw/predict-stata.dta")
+predict_stata <- as.data.frame(predict_stata)
+
+## Remove all Stata-related attributes
+attr(predict_stata, "label") <- NULL
+for (var in colnames(predict_stata)) {
+  attr(predict_stata[[deparse(as.name(var))]], "label") <- NULL
+  attr(predict_stata[[deparse(as.name(var))]], "format.stata") <- NULL
+}
+
+## Save file to be used in the package
+usethis::use_data(predict_stata, overwrite = TRUE, internal = TRUE)
---
+++ data-raw/predict-stata.do
@@ -0,0 +1,31 @@
+cd "~/R-dev/streg/"
+
+set more off
+webuse cancer, clear
+
+streg drug, dist(exp)
+predict mean_exp, mean
+predict median_exp, median
+predict surv_exp, surv
+predict h_exp, hazard
+predict hr_exp, hr
+predict xb_exp, xb
+
+streg drug, dist(wei)
+predict mean_wei, mean
+predict median_wei, median
+predict surv_wei, surv
+predict h_wei, hazard
+predict hr_wei, hr
+predict xb_wei, xb
+
+streg drug, dist(gom)
+predict median_gom, median
+predict surv_gom, surv
+predict h_gom, hazard
+predict hr_gom, hr
+predict xb_gom, xb
+
+drop studytime died drug age _*
+compress
+save data-raw/predict-stata, replace
