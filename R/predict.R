2d731f1b2bd1a803f173566b058803d11b7a71f2
Alessandro Gasparini <ellessenne@gmail.com>
2020-11-27 15:27:06 +0100

    Updated documentation for predict function (#3)

--- R/predict.R
+++ R/predict.R
@@ -1,11 +1,45 @@
+#' Predict Method for Parametric Proportional Hazards Survival Models
+#' @param object A fitted [streg()] object.
+#' @param newdata Optionally, a data frame in which to look for variables with which to predict.
+#' If `NULL` (the default), predictions will be computed for the data used to fit the model.
+#' In this scenario, passing an [streg()] model fitted with the argument `x = TRUE` is required.
+#' @param type The type of prediction required.
+#' The default `"xb"` is to return the linear predictor.
+#' Other possible predictions are the hazard function (`type = "hazard"`) and the survival function (`type = "survival"`).
+#' @param se.fit Logical switch indicating if standard errors are required.
+#'
+#' @details If `newdata` is omitted the predictions are based on the data used for the fit.
+#'
+#' @return If `se.fit = FALSE`, a vector with the required predictions.
+#' If `se.fit = TRUE` a `data.frame` is returned with a column for the predictions (named `fit`) and a column for the fitted standard errors (named `se.fit`).
+#'
+#' @note Standard errors are estimated using the numerical delta method, as implemented in [rstpm2::predictnl()].
+#' This is equivalent (in principle) to Stata's `predictnl` command.
+#'
 #' @importFrom stats predict
+#'
 #' @export
+#'
+#' @examples
+#'
+#' library(streg)
+#' data("kva", package = "streg")
+#'
+#' # Fit a model
+#' fit <- streg(Surv(failtime, event) ~ load + bearings, data = kva, distribution = "exp", x = TRUE)
+#'
+#' # Predict the hazard for each subject
+#' predict(fit, type = "surv")
+#'
+#' # Predict the survival probability, including a standard error for the fitted value
+#' predict(fit, type = "surv", se.fit = TRUE)
+#'
+#' # Predictions for a new dataset
+#' nd <- data.frame(failtime = 30, event = 1, load = 10, bearings = 3)
+#' predict(fit, newdata = nd, type = "surv")
+#' predict(fit, newdata = nd, type = "surv", se.fit = TRUE)
+#'
 predict.streg <- function(object, newdata = NULL, type = "xb", se.fit = FALSE, ...) {
-  # object <- fit3
-  # newdata <- NULL
-  # type <- "xb"
-  # se.fit <- FALSE
-
   # Process type of prediction to obtain
   type <- match.arg(arg = type, choices = c("xb", "hazard", "surv"))
 
